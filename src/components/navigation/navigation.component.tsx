8794b6fa8a42d6c0e8bf002bb6f8dd03fa508d70
AllanGame <allangameyt28@gmail.com>
2022-09-09 22:12:01 -0500

    Add geniuses icon slider in navbar

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,39 +1,63 @@
-import { Navbar, Text } from "@nextui-org/react";
+import { Avatar, Grid, Navbar, Spacer, Text } from "@nextui-org/react";
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from "react";
 
 function NavbarLink({
   href,
-  text,
-  gradient,
-  bold = false
-}: { href: string, text: string, gradient?: string, bold?: boolean }) {
+  text
+}: { href: string, text: string }) {
   const router = useRouter();
-  const style = gradient !== undefined ? { textGradient: gradient } : undefined;
   return (
     <NextLink href={href}>
-      <Navbar.Link  
+      <Navbar.Link
         isActive={router.pathname === href}
-        variant="underline"
-        css={style}>
-        {bold ? <Text b>{text}</Text> : text}
+        variant="underline">
+        {text}
       </Navbar.Link>
-    </NextLink>
+    </NextLink >
   )
 }
 
 function Navigation() {
+  const router = useRouter();
+
+  const [avatarIndex, setAvatarIndex] = useState(0);
+  const geniusesAvatars = [
+    "https://upload.wikimedia.org/wikipedia/commons/e/e9/Albert-einstein-profile-picture-512x512.png.cf.png",
+    "https://play-lh.googleusercontent.com/OGmSJWcggXr5ZAn9tg0LqQJ6A_STanYzJWUP4nLD7lLSxcF2KXLx_PuaY3AjchAh9Uo",
+    "https://m.media-amazon.com/images/I/71N9j7P8Q8L._AC_SL1260_.jpg"
+  ]
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAvatarIndex((prevIndex) => (prevIndex + 1) % geniusesAvatars.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Navbar isBordered isCompact variant="floating">
       <Navbar.Content />
       <Navbar.Content>
         <NavbarLink href="/" text="Inicio" />
         <NavbarLink href="/herramientas" text="Herramientas" />
-        <NavbarLink href="/genios" text="Genios" gradient="45deg, $blue600 -20%, $pink600 50%" bold />
+        <NextLink href="/genios">
+          <Navbar.Link
+            isActive={router.pathname === "/genios"}
+            variant="underline"
+            css={{ textGradient: "45deg, $blue600 -20%, $pink600 50%", padding: 0, margin: 0 }}>
+            <Grid xs>
+              <Avatar size="sm" squared bordered color="success" src={geniusesAvatars[avatarIndex]} />
+              <Spacer x={0.5} />
+            </Grid>
+            <Text b>Genios</Text>
+          </Navbar.Link>
+        </NextLink>
         <NavbarLink href="/recursos" text="Recursos" />
       </Navbar.Content>
       <Navbar.Content />
-    </Navbar>
+    </Navbar >
   )
 }
 
